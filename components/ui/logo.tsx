97187baece070edefb93161a0e84d27a2114929f
tprimesolution <tprimesolution@gmail.com>
2025-02-26 02:53:54 +0530

    logo change

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import logo from "@/public/images/logo.svg";
+import logo from "@/public/images/technoprime-logo.jpeg";
 
 export default function Logo() {
   return (
