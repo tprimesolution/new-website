cf6bd551606078927f1f85f25ba6c5c1c5a28626
tprimesolution <tprimesolution@gmail.com>
2025-02-26 18:11:11 +0530

    logo

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import Logo from "./logo";
+export default function Header({ show = false }) {
+  if (!show) return null;
 
-export default function Header() {
   return (
     <header className="z-30 mt-2 w-full md:mt-5">
       <div className="mx-auto max-w-6xl px-4 sm:px-6">
-        <div className="relative flex h-14 items-center justify-center rounded-2xl bg-gray-900/90 px-3 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,var(--color-gray-800),var(--color-gray-700),var(--color-gray-800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] after:absolute after:inset-0 after:-z-10 after:backdrop-blur-xs">
-          {/* Centered Logo */}
+        <div className="relative flex h-14 items-center justify-center rounded-2xl bg-gray-900/90 px-3">
           <div className="flex items-center gap-2">
             <Logo />
             <span className="text-white text-lg font-semibold"></span>
