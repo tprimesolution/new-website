f59b151208c75340de512394b33a163e7a1633c6
tprimesolution <tprimesolution@gmail.com>
2025-02-26 18:29:17 +0530

    next line

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -9,13 +9,14 @@ export default function HeroHome() {
         <div className="py-12 md:py-20">
           {/* Section header */}
           <div className="pb-12 text-center md:pb-20">
-            <h1
-              className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,var(--color-gray-200),var(--color-indigo-200),var(--color-gray-50),var(--color-indigo-300),var(--color-gray-200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-4xl font-semibold text-transparent md:text-5xl"
-              data-aos="fade-up"
-            >
-              TechnoPrime <>br</>
-              Forge the Exceptional
-            </h1>
+          <h1
+            className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,var(--color-gray-200),var(--color-indigo-200),var(--color-gray-50),var(--color-indigo-300),var(--color-gray-200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-4xl font-semibold text-transparent md:text-5xl"
+            data-aos="fade-up"
+          >
+            TechnoPrime <br />
+            Forge the Exceptional
+          </h1>
+
             <div className="mx-auto max-w-3xl">
               <p
                 className="mb-8 text-xl text-indigo-200/65"
